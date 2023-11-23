250b53b630c51142b5dfc5853757b691136b75d3
Ryan Mebruer <rmebruer@gmail.com>
2023-11-22 20:46:13 -0500

    Update navbar

---
+++ src/LandingPage/Navigation.js
@@ -0,0 +1,22 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href='/'></a>Beef</li>
+                <li><a href='/'></a>Contact</li>
+                <li><a href='/'></a>Merchandise</li>
+            </ul>
+        </nav>
+    );
+};
+
+const Logo = () => {
+    return (
+        <>
+            <img alt="Lost Loop Logo" />
+        </>
+    );
+    ;
+}
+
+export default NavBar;
