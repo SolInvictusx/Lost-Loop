95b9a763dc160b25543b4a8e18cc4387df1b764f
Ryan Mebruer <rmebruer@gmail.com>
2023-11-26 21:42:29 -0500

    Update nav

--- src/LandingPage/Navigation.js
+++ src/LandingPage/Navigation.js
@@ -5,13 +5,13 @@ const NavBar = () => {
         <nav className='nav'>
             <a href='/'><Logo /></a>
             <ul className='nav-list'>
-                <li>
+                <li className='nav-link'>
                     <a href='/'>Beef</a>
                 </li>
-                <li>
+                <li className='nav-link'>
                     <a href='/'>Contact</a>
                 </li>
-                <li>
+                <li className='nav-link'>
                     <a href='/'>Merchandise</a>
                 </li>
             </ul>
@@ -25,7 +25,10 @@ const Logo = () => {
         <div>
             <img src={LLLogo}
                 style={{ height: 50, width: 50 }}
-                alt='Lost Loop Logo' />
+                alt='Lost Loop Logo'
+                className='logo-img'
+            />
+
         </div>
     );
     ;
