c60fe0c2dc7b7c7790869da6000d35cb52b273b8
Ryan Mebruer <rmebruer@gmail.com>
2023-12-02 20:40:52 -0500

    Fixing navigation

---
+++ src/OtherPages/BeefMain.js
@@ -0,0 +1,14 @@
+import AmountBeef from "../BeefPage/Quantity";
+import NavBar from "../LandingPage/Navigation";
+
+
+const BeefPage = () => {
+    return (
+        <div>
+            <NavBar />
+            <AmountBeef />
+        </div>
+    );
+};
+
+export default BeefPage;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './css/styles.css';
+import BeefPage from './OtherPages/BeefMain';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "beef",
+    element: <BeefPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
