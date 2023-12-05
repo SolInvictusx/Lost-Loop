1c3b8ef81db04708665cc8afd588f177a7362a3c
Ryan Mebruer <rmebruer@gmail.com>
2023-12-04 23:27:40 -0500

    Update beef page

--- src/BeefPage/CutsArray.js
+++ src/BeefPage/CutsArray.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import image1 from '../img/GroundBeef.jpg';
 import image2 from '../img/Sirloin.jpg';
 import image3 from '../img/KCStrip.jpeg';
@@ -5,24 +6,29 @@ import image4 from '../img/ChuckRoast.jpg';
 import image5 from '../img/RumpRoast.jpg';
 import image6 from '../img/FiletMignon.jpg';
 import image7 from '../img/CubeSteak.jpg';
-import image8 from '../img/SirloinTipRoast.jpg'
+import image8 from '../img/SirloinTipRoast.jpg';
 
 const BeefArray = () => {
     const imgArray = [
-        image1,
-        image2,
-        image3,
-        image4,
-        image5,
-        image6,
-        image7,
-        image8,
+        { src: image1, text: 'Ground Beef' },
+        { src: image2, text: 'Sirloin Steak' },
+        { src: image3, text: 'KC Strip' },
+        { src: image4, text: 'Chuck Roast' },
+        { src: image5, text: 'Rump Roast' },
+        { src: image6, text: 'Filet Mignon' },
+        { src: image7, text: 'Cube Steak' },
+        { src: image8, text: 'Sirloin Tip Roast' },
     ];
 
     return (
         <div className="img-gallery">
-            {imgArray.map((image, index) => (
-                <img key={index} src={image} alt={`Image ${index + 1}`} className="gallery-img" />
+            {imgArray.map((item, index) => (
+                <div key={index} className="gallery-item">
+                    <div className='image-container'>
+                        <img src={item.src} alt={`Image ${index + 1}`} className="gallery-img" />
+                    </div>
+                    <div className="image-text">{item.text}</div>
+                </div>
             ))}
         </div>
     );
--- src/BeefPage/InfoSection.js
+++ src/BeefPage/InfoSection.js
@@ -1,14 +1,22 @@
 const InfoPortion = () => {
     return (
         <div>
-            <h1>
-                Portion Sizing!
-            </h1>
+            <div>
+                <h1>
+                    Portion Sizing!
+                </h1>
+                <h2>
+                    We offer portion sizes ranging from 1/16 beef up to the entire beef. When portioned out in percentages, independent of what portion size our customers select, we provide
+                    50% in hamburger, 25% in roast, and the remaining 25% in steaks. We accomadate special requests and customization of these perecentages as well as the cuts of steak! Listed below
+                    are the cuts of beef that we offer!
+                </h2>
+            </div>
             <h2>
-                We offer portion sizes ranging from 1/16 beef up to the entire beef. When portioned out in percentages, independent of what portion size our customers select, we provide
-                50% in hamburger, 25% in roast, and the remaining 25% in steaks. We accomadate special requests and customization of these perecentages as well as the cuts of steak! Listed below
-                are the cuts of beef that we offer!
+                Interested? Click the button below to contact us to claim your beef today!
             </h2>
+            <button>
+                Contact Us!
+            </button>
         </div>
     );
 };
