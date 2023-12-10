cbd6e3fb38191a816e00baba688f5c17b88d50da
Ryan Mebruer <rmebruer@gmail.com>
2023-12-09 23:47:52 -0500

    Finished form

--- src/ContactPage/FormComponent.js
+++ src/ContactPage/FormComponent.js
@@ -1,16 +1,30 @@
-
+import { useState } from "react";
 
 const FormComponent = () => {
+    const [data, setData] = useState({ firstname: "", lastname: "", email: "", subject: "" });
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setData({ ...data, [name]: value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(data);
+    }
+
     return (
-        <form>
+        <form method="post" onSubmit={handleSubmit}>
             <h1>Contact <span>Here</span></h1>
-            <input type="text" name="first-name" id="" placeholder="First Name" />
-            <input type="text" name="last-name" id="" placeholder="Last Name" />
-            <input type="email" name="email" id="" placeholder="Email" />
-            <input type="text" name="subject" id="" placeholder="Subject" />
-            <textarea name="message" id="" cols="30" rows="10" placeholder="Message" />
+            <input type="text" name="firstname" id="" onChange={handleChange} value={data.firstname} placeholder="First Name" />
+            <input type="text" name="lastname" id="" onChange={handleChange} value={data.lastname} placeholder="Last Name" />
+            <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder="Example@gmail.com" />
+            <input type="text" name="subject" id="" onChange={handleChange} value={data.subject} placeholder="Subject" />
+            <textarea name="message" id="" cols="30" onChange={handleChange} value={data.message} rows="10" placeholder="Message" />
             <button type="submit">Submit</button>
         </form>
+
+
     )
 }
 
