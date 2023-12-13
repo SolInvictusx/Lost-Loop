e203d00cb58b5a42e4e97348c8fe6e4988b7577e
Ryan Mebruer <rmebruer@gmail.com>
2023-12-12 21:01:59 -0500

    Update responsive landing page

--- src/ContactPage/FormComponent.js
+++ src/ContactPage/FormComponent.js
@@ -16,11 +16,11 @@ const FormComponent = () => {
     return (
         <form method="post" onSubmit={handleSubmit}>
             <h1>Contact <span>Here</span></h1>
-            <input type="text" name="firstname" id="" onChange={handleChange} value={data.firstname} placeholder="First Name" />
-            <input type="text" name="lastname" id="" onChange={handleChange} value={data.lastname} placeholder="Last Name" />
-            <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder="Example@gmail.com" />
-            <input type="text" name="subject" id="" onChange={handleChange} value={data.subject} placeholder="Subject" />
-            <textarea name="message" id="" cols="30" onChange={handleChange} value={data.message} rows="10" placeholder="Message" />
+            <input type="text" name="firstname" id="" onChange={handleChange} value={data.firstname} placeholder="First Name" className="form-text" />
+            <input type="text" name="lastname" id="" onChange={handleChange} value={data.lastname} placeholder="Last Name" className="form-text" />
+            <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder="Example@gmail.com" className="form-text" />
+            <input type="text" name="subject" id="" onChange={handleChange} value={data.subject} placeholder="Subject" className="form-text" />
+            <textarea name="message" id="" cols="30" onChange={handleChange} value={data.message} rows="10" placeholder="Message" className="form-text" />
             <button type="submit">Submit</button>
         </form>
 
