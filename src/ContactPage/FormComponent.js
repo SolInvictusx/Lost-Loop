ef17e133c2f9d03a73bdc923786c890d5814ea45
Ryan Mebruer <rmebruer@gmail.com>
2023-12-14 22:08:40 -0500

    Update form section

--- src/ContactPage/FormComponent.js
+++ src/ContactPage/FormComponent.js
@@ -16,11 +16,11 @@ const FormComponent = () => {
     return (
         <form method="POST" onSubmit={handleSubmit} id="contact-form" name="contact" netlify >
             <h1>Contact <span>Here</span></h1>
-            <input type="text" name="firstname" id="" onChange={handleChange} value={data.firstname} placeholder="First Name" className="form-text" />
-            <input type="text" name="lastname" id="" onChange={handleChange} value={data.lastname} placeholder="Last Name" className="form-text" />
-            <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder="Example@gmail.com" className="form-text" />
-            <input type="text" name="subject" id="" onChange={handleChange} value={data.subject} placeholder="Subject" className="form-text" />
-            <textarea name="message" id="" cols="30" onChange={handleChange} value={data.message} rows="10" placeholder="Message" className="form-text" />
+            <label><input type="text" name="firstname" id="" onChange={handleChange} value={data.firstname} placeholder="First Name" className="form-text" /></label>
+            <label><input type="text" name="lastname" id="" onChange={handleChange} value={data.lastname} placeholder="Last Name" className="form-text" /></label>
+            <label><input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder="Example@gmail.com" className="form-text" /></label>
+            <label><input type="text" name="subject" id="" onChange={handleChange} value={data.subject} placeholder="Subject" className="form-text" /></label>
+            <label><textarea name="message" id="" cols="30" onChange={handleChange} value={data.message} rows="10" placeholder="Message" className="form-text" /></label>
             <button type="submit">Submit</button>
         </form>
 
