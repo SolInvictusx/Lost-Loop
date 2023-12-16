660b9a7cc1eba9e3cf60aeba010217950de187e5
Ryan Mebruer <rmebruer@gmail.com>
2023-12-16 13:38:43 -0500

    Update contact page

--- src/ContactPage/FormComponent.js
+++ src/ContactPage/FormComponent.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const FormComponent = () => {
+    const [submissionResult, setSubmissionResult] = useState(null);
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -10,7 +12,7 @@ const FormComponent = () => {
         try {
             const response = await fetch(form.action, {
                 method: form.method,
-                body: new URLSearchParams(formData), // Use URLSearchParams to format the data
+                body: new URLSearchParams(formData),
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                 },
@@ -19,34 +21,44 @@ const FormComponent = () => {
             if (response.ok) {
                 // Handle success
                 console.log('Form submitted successfully');
-                // Update your UI with a success message
+                setSubmissionResult('success');
             } else {
                 // Handle error
                 console.error('Form submission failed');
-                // Update your UI with an error message
+                setSubmissionResult('error');
             }
         } catch (error) {
             console.error('An error occurred during form submission', error);
-            // Update your UI with an error message
+            setSubmissionResult('error');
         }
     };
 
     return (
-        <form
-            method="POST"
-            action="/contact"
-            name="contact"
-            onSubmit={handleSubmit} // Attach the submit handler
-        >
-            <input type="hidden" name="form-name" value="contact" />
-            <h1>Contact <span>Here</span></h1>
-            <input required type="text" name="firstname" placeholder="First Name" className="form-text" />
-            <input required type="text" name="lastname" placeholder="Last Name" className="form-text" />
-            <input required type="email" name="email" placeholder="Example@gmail.com" className="form-text" />
-            <input required type="text" name="subject" placeholder="Subject" className="form-text" />
-            <textarea required name="message" cols="30" rows="10" placeholder="Message" className="form-text"></textarea>
-            <button type="submit">Submit</button>
-        </form>
+        <div>
+            <form
+                method="POST"
+                action="/contact"
+                name="contact"
+                onSubmit={handleSubmit}
+            >
+                <input type="hidden" name="form-name" value="contact" />
+                <h1>Contact <span>Here</span></h1>
+                <input required type="text" name="firstname" placeholder="First Name" className="form-text" />
+                <input required type="text" name="lastname" placeholder="Last Name" className="form-text" />
+                <input required type="email" name="email" placeholder="Example@gmail.com" className="form-text" />
+                <input required type="text" name="subject" placeholder="Subject" className="form-text" />
+                <textarea required name="message" cols="30" rows="10" placeholder="Message" className="form-text"></textarea>
+                <button type="submit">Submit</button>
+            </form>
+
+            {submissionResult === 'success' && (
+                <p style={{ color: 'green' }}>Form submitted successfully!</p>
+            )}
+
+            {submissionResult === 'error' && (
+                <p style={{ color: 'red' }}>Form submission failed. Please try again.</p>
+            )}
+        </div>
     );
 };
 
